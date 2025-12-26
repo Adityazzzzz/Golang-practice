c4ec328d42ba2317f406d8516465f71a6cb07056
Aditya Singh <adityasinghrajawat2004@gmail.com>
2025-12-26 14:44:14 +0530

    If-else, switch

--- 2-simpleValues/main.go
+++ 2-simpleValues/main.go
@@ -1,6 +1,7 @@
 package main
 
 import "fmt"
+import "time"
 
 func main() {
 	//DATA TYPES
@@ -24,13 +25,11 @@ func main() {
 		fmt.Println(name3)
 	}
 
-
 	//CONSTANTS : "const"
 	{
 		//1
 		const age = 10
 		fmt.Println(age)
-
 		//2
 		const (
 			port =5000
@@ -38,4 +37,54 @@ func main() {
 		fmt.Println(port)
 	}
 
+	//LOOPS
+	{
+		i:=1
+		for i<=3{
+			//print
+			i = i+1
+		}
+		//or
+		for i:=0;i<=3;i++ {
+			//print
+			continue
+		}
+	}
+
+	//IF-ELSE
+	{
+		num:=1
+		if num>0 {
+			fmt.Println(num)
+		}else {
+			fmt.Println(num)
+		}
+	}
+
+	//SWITCH
+	{
+		j:=1
+		switch j{
+			case 1: fmt.Println(1)
+			case 2: 
+			default:
+		}
+		
+		// Multiple switch
+		switch time.Now().Weekday(){
+		case time.Thursday:
+			fmt.Println("its thursday")
+		}
+
+		// Type Switch
+		whoAmI := func(i interface{}){
+			switch t := i.(type){
+				case int:
+					fmt.Println("i is int",t)
+				case string:
+					fmt.Println("i is string",t)
+			}
+		}
+		whoAmI(50)
+	}
 }
